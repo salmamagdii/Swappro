3d3a730b23108b11a50b078e6e3002ed30b3fc32
salmamagdii <salma.magdy@stu.eng.aswu.edu.eg>
2025-06-03 18:06:47 +0300

    Update frontend UI and logic

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.post('/signin', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
